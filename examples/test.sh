4e5cea4b9117568be37ea07e3e82bff132d0a78e
boundino <boundino@gmail.com>
2023-09-24 07:11:21 +0200

    fix count for json

--- examples/test.sh
+++ examples/test.sh
@@ -2,6 +2,9 @@ set -x
 
 python3 hltcount.py --lumiranges 373710:100:150 --pathnames HLT_ZeroBias_v8,HLT_PPRefGEDPhoton40_v1,HLT_AK4PFJet100_v1
 
+certjson=/eos/user/c/cmsdqm/www/CAF/certification/Collisions22/Collisions2022HISpecial/Cert_Collisions2022HISpecial_362293_362323_Golden.json
+[[ -f $certjson ]] && python3 hltcount.py --lumiranges $certjson --pathnames HLT_HIMinimumBias_v2
+
 python3 hltrunsummary.py --run 373710 --pathnames HLT_ZeroBias_v8,HLT_PPRefGEDPhoton40_v1,HLT_AK4PFJet100_v1
 
 python3 hlttime.py --timemin 2023-09-19T18:00:00 --timemax 2023-09-20
--- hltcount.py
+++ hltcount.py
@@ -16,24 +16,24 @@ def getcount(runlumijson, path, omsapi = omsapi):
             print("\033[31merror: bad path name or run number: \"\033[4m" + path + ", " + run + "\033[0m\033[31m\", skip it..\033[0m")
             continue
 
-        lumimin = runlumijson[run]["min"]
-        lumimax = runlumijson[run]["max"]
+        for ls in runlumijson[run]:
+            lumimin = ls[0]
+            lumimax = ls[1]
         
-        if lumimin >= 0:
-            q.filter("first_lumisection_number", lumimin, "GE")
-        if lumimax >= 0:
-            q.filter("last_lumisection_number", lumimax, "LE")
+            if lumimin >= 0:
+                q.filter("first_lumisection_number", lumimin, "GE")
+            if lumimax >= 0:
+                q.filter("last_lumisection_number", lumimax, "LE")
 
-        datajson = q.data().json()
-        for ls in datajson["data"]:
-            # print(ls["attributes"]["first_lumisection_number"])
-            totalcount += ls["attributes"]["counter"]
+            datajson = q.data().json()
+            for ls in datajson["data"]:
+                totalcount += ls["attributes"]["counter"]
 
     return totalcount
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = 'Print HLT counts in given lumi ranges of runs')
-    parser.add_argument('--lumiranges', required = True, help = 'e.g. 373664:25:30,373710 => <run>(:<minLS>:<maxLS>)')
+    parser.add_argument('--lumiranges', required = True, help = '<run>(:<minLS>:<maxLS>) e.g. 373664:25:30,373710 || (option 2) cert json file')
     parser.add_argument('--pathnames', required = True, help = 'e.g. HLT_ZeroBias_v8,HLT_PPRefL1SingleMu7_v1')
     parser.add_argument('--outcsv', required = False, help = 'Optional csv output file')
     args = parser.parse_args()
@@ -43,31 +43,44 @@ if __name__ == "__main__":
 
     lumiRangesStr = args.lumiranges.split(",")
     runlumi = {}
-    if len(lumiRangesStr) == 1 and lumiRangesStr[0].endswith(".txt") :
-        text_file = open(lumiRangesStr[0], "r")
-        lines = text_file.read().splitlines()
-        lumiRangesStr = lines
-    for str in lumiRangesStr:
-        lumistr = str.split(":")
-        if len(lumistr) == 1:
-            lumistr.extend(['-1', '-1'])
-        elif len(lumistr) != 3:
-            print("\033[31merror: bad lumi range: \"\033[4m" + str + "\033[0m\033[31m\", skip it..\033[0m")
-            continue
-        thismin = int(lumistr[1])
-        thismax = int(lumistr[2])
-        if thismin > thismax: 
-            print("\033[31merror: bad lumi range: \"\033[4m" + str + "\033[0m\033[31m\", skip it..\033[0m")
-            continue
+    if len(lumiRangesStr) == 1 and lumiRangesStr[0].endswith(".json") :
+        with open(lumiRangesStr[0]) as ff:
+            runlumi = json.load(ff)
+    else:
+        if len(lumiRangesStr) == 1 and lumiRangesStr[0].endswith(".txt") :
+            text_file = open(lumiRangesStr[0], "r")
+            lines = text_file.read().splitlines()
+            lumiRangesStr = lines
+        for str in lumiRangesStr:
+            lumistr = str.split(":")
+            thisrun = lumistr[0]
+            if len(lumistr) == 1:
+                lumistr.extend(['-1', '-1'])
+            elif len(lumistr) != 3:
+                print("\033[31merror: bad lumi range: \"\033[4m" + str + "\033[0m\033[31m\", skip it..\033[0m")
+                continue
+            thismin = int(lumistr[1])
+            thismax = int(lumistr[2])
+            if thismin > thismax: 
+                print("\033[31merror: bad lumi range: \"\033[4m" + str + "\033[0m\033[31m\", skip it..\033[0m")
+                continue
 
-        if lumistr[0] not in runlumi:
-            newele = {"min" : thismin, "max" : thismax}
-            runlumi[lumistr[0]] = newele
-        else:
-            if thismin < runlumi[lumistr[0]]["min"] or thismin < 0:
-                runlumi[lumistr[0]]["min"] = thismin
-            if (thismax > runlumi[lumistr[0]]["max"] and runlumi[lumistr[0]]["max"] >= 0) or thismax < 0:
-                runlumi[lumistr[0]]["max"] = thismax
+            if thisrun not in runlumi:
+                newele = [thismin, thismax]
+                runlumi[thisrun] = []
+                runlumi[thisrun].append(newele)
+            else:
+                treat = 0
+                for ls in runlumi[thisrun]:
+                    if (thismin <= ls[0] or thismin < 0) and (thismax >= ls[0]):
+                        ls[0] = thismin
+                        treat = 1
+                    if (thismax >= ls[1] or thismax < 0) and (thismin <= ls[1]):
+                        ls[1] = thismax
+                        treat = 1
+                if treat == 0:
+                    newele = [thismin, thismax]
+                    runlumi[thisrun].append(newele)
 
     print("Summing up lumi sections: \033[4;32;1m", end = "")
     print(runlumi, end = "")
