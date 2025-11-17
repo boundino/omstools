da974fe03183dcf228a0224d62d21e9e7a652ba5
boundino <boundino@gmail.com>
2025-11-17 19:00:51 +0100

    compare.cc: add MAXRATE option to allow for different maxyaxis

--- compare/compare.cc
+++ compare/compare.cc
@@ -16,17 +16,20 @@ int macro(std::string inputconfig) {
   auto leg = new TLegend(x, y-inputs.size()*0.048, x+0.15, y);
   xjjroot::setleg(leg, 0.038);
   for (auto ii : inputs) {
-    // auto isplit = xjjc::str_divide_trim(ii, ":");
     auto i = lists.size();
     auto gd = new getdata(ii, xjjroot::mycolor_satmiddle[ccs[i]]);
     lists.push_back(gd);
     leg->AddEntry(gd->g(), gd->tleg().c_str(), "p");
   }
-  
-  auto hempty = new TH2F("hempty", ";Instantaneous luminosity [10^{33} cm^{-2} s^{-1}]; Rate [kHz]", 10, 0, 6.5e-6, 10, 0, 70);
+
+  float maxy = conf.has("MAXRATE") ? conf.get<float>("MAXRATE") : 70.;
+  auto hempty = new TH2F("hempty", ";Instantaneous luminosity [10^{33} cm^{-2} s^{-1}]; Rate [kHz]", 10, 0, 6.5e-6, 10, 0, maxy);
   xjjroot::sethempty(hempty, 0, -0.2);
   xjjroot::setgstyle(1);
   xjjroot::adjustmargin(1, 2.5, 1, 0.7);
+  if (maxy > 100)
+    hempty->GetYaxis()->SetMaxDigits(2);
+  
   auto c = new TCanvas("c", "", 600, 600);
   hempty->Draw("axis");
   for (const auto& l : lists)
