120c56421e6264e8acde3bc56262da26ce48fa23
boundino <boundino@gmail.com>
2026-06-01 17:05:23 +0200

    restructure local macrosc

--- toolkit/update_web.sh
+++ toolkit/update_web.sh
@@ -1,5 +1,5 @@
 python3 runfill4web.py 
-cd ../cms-hin-coordination/webs/public/
-git diff datasets/HIRun2025A/
-git add datasets/HIRun2025A/*
+cd ../../cms-hin-coordination/webs/public/
+git diff datasets/HIRun2026A/
+git add datasets/HIRun2026A/*
 cd -
